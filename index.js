9bcb7030d640c1d3374f06ef0a74e87b0d383e8d
(mavin) <ddw6229@naver.com>
2023-10-16 14:24:38 +0900

    <index.js> 업데이트 주석추가

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// 출처: youngwanho(mavin) 개인 작업물
+// 업데이트: 2023.10.15
+// 탭메뉴 자료입니다.
+
 const tabListItem = document.querySelectorAll(".tab-list-item")
 const tabPanel= document.querySelectorAll(".tab-panel")
 
